98b6d5d58da4577a5ca0434d7612cc4621ddb734
IMDAD <64931177+imdadhaq@users.noreply.github.com>
2020-11-30 14:55:52 +0600

    LAB TASK-8

---
+++ Faculty.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_task_11._30._2020
+{
+     class Faculty
+    {
+        
+        public string Name { get; set; }
+        public string Id { get; set; }
+        public Faculty() { }
+        public Faculty(string Name, string Id)
+        {
+           this.Name = Name;
+            this.Id = Id;
+        }
+        public void Show()
+        {
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("Id: " + Id);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using Lab_task_11._30._2020;
+
+namespace OOP2FinalLabTask
+{
+    class Dept
+
+    {
+        public string Name { get; set; }
+        public string Id { get; set; }
+
+        ArrayList faculties;
+        public Dept(string name, string id)
+        {
+            Name = name;
+            Id = id;
+           
+        }
+        static void Main(string[] args)
+        {
+            Faculty f = new Faculty("IMDAD","000212");
+
+            ArrayList list = new ArrayList();
+            list.Add(f);
+            list.Add(10);
+
+            f.Show();
+
+        }
+
+       
+        
+        public void AddFaculty(params Faculty[] faculties)
+        {
+            foreach (Faculty f in faculties)
+            {
+                this.faculties.Add(f);
+            }
+        }
+       
+
+ 
+}
